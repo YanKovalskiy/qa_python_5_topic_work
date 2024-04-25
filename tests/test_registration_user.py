c2d00da3728f00685c119bd3f47915612321c7c5
Vladimir Yankovskiy <vyankovskiy@gmail.com>
2024-04-25 18:54:39 +0500

    cleaning code

--- tests/test_registration_user.py
+++ tests/test_registration_user.py
@@ -1,5 +1,4 @@
 import time
-
 import stellarburgers as sb
 from locators import *
 from selenium.webdriver.support import expected_conditions as ec
@@ -9,12 +8,13 @@ from selenium.webdriver.support.wait import WebDriverWait as WDWait
 class TestRegistrationUser:
     def test_registration_form_positive_test(self, web_drv, email, password):
         sb.registration_user(web_drv, 'Владимир', email, password)
-        WDWait(web_drv, 10).until(ec.element_to_be_clickable(LINK_PERSONAL_ACCOUNT_IN_HEADER)).click()
+        wait = WDWait(web_drv, 10)
+        wait.until(ec.element_to_be_clickable(LINK_PERSONAL_ACCOUNT_IN_HEADER)).click()
         sb.login_user(web_drv, email, password)
         time.sleep(2)
-        WDWait(web_drv, 10).until(ec.element_to_be_clickable(LINK_PERSONAL_ACCOUNT_IN_HEADER)).click()
-        filed_email = WDWait(web_drv, 10).until(ec.visibility_of_element_located(INPUT_FIELD_LOGIN_IN_PERSONAL_ACCOUNT))
-        assert filed_email.get_attribute('value') == email
+        wait.until(ec.element_to_be_clickable(LINK_PERSONAL_ACCOUNT_IN_HEADER)).click()
+        assert wait.until(ec.visibility_of_element_located(
+            INPUT_FIELD_LOGIN_IN_PERSONAL_ACCOUNT)).get_attribute('value') == email
 
     def test_registration_with_incorrect_password(self, web_drv, email):
         sb.registration_user(web_drv, 'Владимир', email, '12345')
